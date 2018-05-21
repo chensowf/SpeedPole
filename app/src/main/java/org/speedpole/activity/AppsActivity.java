d14c86dbb6ed17f0b111d51c02f03812ad315dda
chen <my@qq.com>
2018-05-21 22:20:38 +0800

    first add

---
+++ app/src/main/java/org/speedpole/activity/AppsActivity.java
@@ -0,0 +1,15 @@
+package org.speedpole.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.speedpole.R;
+
+public class AppsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apps);
+    }
+}
